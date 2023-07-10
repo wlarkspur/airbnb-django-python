39865b4c02a3b69ca09dffcfc70a4a42188fd90a
wlarkspur <newacon@naver.com>
2023-07-10 22:59:08 +0200

    Amazing Django admin functions!

--- README.md
+++ README.md
@@ -11,5 +11,13 @@
 3. python manage.py migrate
 4. python manage.py createsuperuser -> admin 계정생성
 5. python manage.py startapp AAA
+ #Model 내용 수정후 DB에 그사실을 알려줘야 한다. // 아래 2단계 매우 중요 !
+6. python manage.py makemigrations  (migration 생성)
+7. python manage.py migrate (migrate)  
+
+Application: 데이터와 데이터의 로직이 있는 섬같은 개념.
+models.py 어플리케이션에 있는 데이터의 정의나 설명을 적는 곳.
+Django는 SQL데이터베이스 언어를 사용한다.(코드를 SQL로 자동 해석해준다.)
+models생성 후에는 admin.py로 가서 생성한 model을 등록시켜야 한다.
 
 - django-admin startproject config .
--- houses/models.py
+++ houses/models.py
@@ -6,8 +6,15 @@ class House(models.Model):
     """Model Definition for House"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(verbose_name="price", help_text="Positive Numbers Only")
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True, help_text="Does this house allow pets?"
+        )
+
+    def __str__(self):
+        return self.name
 
     
