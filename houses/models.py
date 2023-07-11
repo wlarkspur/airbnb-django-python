19c90d87e57a1598cb5acab85bc24b6d92e257b3
wlarkspur <newacon@naver.com>
2023-07-11 20:45:54 +0200

    black formatter installed

--- houses/models.py
+++ houses/models.py
@@ -6,15 +6,13 @@ class House(models.Model):
     """Model Definition for House"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField(verbose_name="price", help_text="Positive Numbers Only")
+    price_per_night = models.PositiveIntegerField(verbose_name='price', help_text="Positive Numbers Only")
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
-        verbose_name="Pets Allowed?",
-        default=True, help_text="Does this house allow pets?"
-        )
+        verbose_name="Pets Allowed?", default=True, help_text="Does this house allow pets?")
 
-    def __str__(self):
+    def __str__(self):  
         return self.name
 
     
